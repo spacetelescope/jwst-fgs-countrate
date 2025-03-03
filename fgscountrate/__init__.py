8a158dcc2149b63d7f8b9170ab17184ec738f469
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-03-03 18:22:47 -0500

    MNT: Stop importing _astropy_init.py

--- fgscountrate/__init__.py
+++ fgscountrate/__init__.py
@@ -1,11 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
 import os
 import sys
 import pkg_resources
