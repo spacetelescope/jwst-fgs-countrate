3e3e9b387ab8675017d3c65f6823f8dac74c65d7
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-03-03 18:21:39 -0500

    Delete fgscountrate/_astropy_init.py

--- fgscountrate/_astropy_init.py
+++
@@ -1,9 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-import os
-
-# Create the test function for self test
-from astropy.tests.runner import TestRunner
-test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
-test.__test__ = False
-__all__ = ['test']
